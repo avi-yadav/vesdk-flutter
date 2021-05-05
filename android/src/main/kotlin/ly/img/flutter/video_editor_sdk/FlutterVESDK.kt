0a749c98791d730b7f76a230808ba89c89b1f651
Avinash Kumar <a0k03y0@homeoffice.wal-mart.com>
2021-05-05 20:27:21 +0530

    changed editor-result-id

--- android/src/main/kotlin/ly/img/flutter/video_editor_sdk/FlutterVESDK.kt
+++ android/src/main/kotlin/ly/img/flutter/video_editor_sdk/FlutterVESDK.kt
@@ -33,7 +33,7 @@ class FlutterVESDK: FlutterIMGLY() {
 
   companion object {
     // This number must be unique. It is public to allow client code to change it if the same value is used elsewhere.
-    var EDITOR_RESULT_ID = 29065
+    var EDITOR_RESULT_ID = 29066
   }
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
